d45d9523a02b9e5f06aeeb38ac62fd1b61912035
Geoffroy Noel <geoffroy.noel@kcl.ac.uk>
2017-01-16 19:44:02 +0000

    Support for dvi outputs.

--- rescale.py
+++ rescale.py
@@ -5,25 +5,25 @@ import re
 #xrandr --output eDP1 --scale 1x1 --pos 320x2400
 
 class Rescaler(object):
-    
+
     def rescale(self, options=None):
         self.options = options or []
-        
+
         self.getDisplaySettings()
-        
+
         self.showDisplaySettings()
 
         self.updateDisplaySettings()
-        
+
         self.showDisplaySettings()
-        
+
         self.applyDisplaySettings()
-    
+
     def showDisplaySettings(self):
         print '-' * 40
         for s in self.settings:
             s['sx'] = 1.0 * s['wspx'] / s['wpx']
-            s['sy'] = 1.0 * s['hspx'] / s['hpx'] 
+            s['sy'] = 1.0 * s['hspx'] / s['hpx']
             print '%(output)s res:%(wpx)sx%(hpx)s pos:%(oxpx)sx%(oypx)s scale:%(sx).1fx%(sy).1f dim:%(wmm)sx%(hmm)s' % s
         print '-' * 40
 
@@ -31,13 +31,13 @@ class Rescaler(object):
         # $ xrandr -q --verbose
         # HDMI2 connected 2880x1620+0+0 (0x4a) normal (normal left inverted right x axis y axis) 510mm x 290mm
         # 1920x1080 (0x4a) 148.500MHz +HSync +VSync *current +preferred
-                
+
         settings = []
-        
+
         info = self.run('xrandr -q --verbose')
-        
+
         for line in info['output'].split('\n'):
-            parts = re.findall(ur'(\w+) connected (\d+)x(\d+)\+(\d+)\+(\d+)\s.+\s(\d+)mm.*?(\d+)mm', line)
+            parts = re.findall(ur'^([\w-]+) connected (\d+)x(\d+)\+(\d+)\+(\d+)\s.+\s(\d+)mm.*?(\d+)mm', line)
             if parts:
                 parts = parts[0]
                 settings.append({
@@ -49,7 +49,7 @@ class Rescaler(object):
                     'wmm': int(parts[5]),
                     'hmm': int(parts[6]),
                 })
-            
+
             parts = re.findall(ur'(\d+)x(\d+)\s.*\s*current', line)
             if parts and settings:
                 parts = parts[0]
@@ -58,22 +58,22 @@ class Rescaler(object):
                     'wpx': int(parts[0]),
                     'hpx': int(parts[1]),
                 })
-                
-            
+
+
         self.settings = settings
 
     def updateDisplaySettings(self):
         #new_settings = []
-        
+
         # get the reference display, the one that is never rescaled
         ref = None
         for s in self.settings:
             if s['output'] in ['eDP1']:
                 ref = s
-        
+
         if ref:
             # rescale the other displays to match the the reference
-            # the objective is that the same size in pixel 
+            # the objective is that the same size in pixel
             # will appear the same size visually.
             # IMPORTANT ASSUMPTION: displays are side by side.
             for s in self.settings:
@@ -88,28 +88,28 @@ class Rescaler(object):
                 # round the scaling to first decimal
                 for f in ['sx', 'sy']:
                     s[f] = round(s[f], 1)
-                
+
                 # experimental: take the smallest scale factor
                 # apply it to both dimensions
                 if 1:
                     s['sy'] = min(s['sx'], s['sy'])
                     s['sy'] = min(2,s['sy'])
                     s['sx'] = s['sy']
-                
+
                 # align to nearest half unit
                 granularity = 0.25
                 if 1:
                     for f in ['sx', 'sy']:
                         s[f] = int(s[f]/granularity)*granularity
-                
+
                 if 'reset' in self.options:
                     for f in ['sx', 'sy']:
                         s[f] = 1
-                
+
                 # recalculate the scaled width and height
                 s['wspx'] = int(s['sx'] * s['wpx'])
                 s['hspx'] = int(s['sy'] * s['hpx'])
-                    
+
             # recalculate the position of each display on the virtual space
             pos = [0, 0]
             for s in sorted(self.settings, key=lambda s: s['oxpx']):
@@ -119,15 +119,15 @@ class Rescaler(object):
 
                 # align each display with the bottom line
                 s['oypx'] = max([s2['hspx'] for s2 in self.settings]) - s['hspx']
-                    
+
         #self.settings = new_settings
-            
+
     def applyDisplaySettings(self):
         if self.settings:
             fb = [
-                sum([s2['wspx'] for s2 in self.settings]), 
+                sum([s2['wspx'] for s2 in self.settings]),
                 max([s2['hspx'] for s2 in self.settings])
-            ] 
+            ]
             command = 'xrandr --fb %dx%d' % (fb[0], fb[1])
             for s in self.settings:
                 # scaling
@@ -135,8 +135,8 @@ class Rescaler(object):
                 s['sx'] = 1.0 * s['wspx'] / s['wpx']
                 s['sy'] = 1.0 * s['hspx'] / s['hpx']
                 s['wfb'] = fb[0]
-                s['hfb'] = fb[1] 
-                # frame buffer size for the entire virtual screen 
+                s['hfb'] = fb[1]
+                # frame buffer size for the entire virtual screen
                 command += '--output %(output)s --mode %(wpx)dx%(hpx)d --scale %(sx).2fx%(sy).2f --panning 0x0+0+0/0x0+0+0/0/0/0/0 --pos %(oxpx)dx%(oypx)d ' % s
             print command
             if 1:
@@ -175,5 +175,5 @@ rescaler.rescale(sys.argv[1:])
 
 # ret = run('xrandr -q')
 # print ret
-    
+
 print 'done'
